1e8a5f15139f86cb21f9b78ef901768c3e311185
exueyuan <1079240024@qq.com>
2020-07-06 11:07:45 +0800

    新增分数循环题答案

---
+++ 各种算法题/分数循环题.py
@@ -0,0 +1,27 @@
+#coding=utf-8
+def getLoopNums(num):
+    startNum = 1
+    yushuList = []
+    while True:
+        # print("开始的数字为:{},余数list为:{}".format(startNum, yushuList))
+        while startNum < num:
+            startNum = startNum * 10
+        if startNum % num == 0:
+            return -1
+        else:
+            yushu = startNum % num
+            if yushu not in yushuList:
+                yushuList.append(yushu)
+                startNum = startNum % num
+            else:
+                index = yushuList.index(yushu)
+                return len(yushuList[index:])
+
+maxLoopNum = -1
+maxLoopNumI = 0
+for i in range(2, 1001):
+    loopNum = getLoopNums(i)
+    if loopNum > maxLoopNum:
+        maxLoopNum = loopNum
+        maxLoopNumI = i
+print(maxLoopNumI, maxLoopNum)
