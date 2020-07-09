cee863f83f41b84fd60d7bc37d833a80070bec06
exueyuan <1079240024@qq.com>
2020-07-09 18:51:30 +0800

    恢复空格的树的解决方法

--- LeetCode算法题/面试题17_13_恢复空格/恢复空格.py
+++ LeetCode算法题/面试题17_13_恢复空格/恢复空格.py
@@ -7,7 +7,8 @@ class Solution(object):
         """
         # dict = {
         #     "a": {"c": {},
-        #           "d": {}},
+        #           "d": {},
+        #           "end"},
         #     "b": {},
         #     "c": {},
         # }
@@ -19,23 +20,42 @@ class Solution(object):
                 if letter not in lastDict:
                     lastDict.setdefault(letter, newDict)
                 if i == len(word)-1:
-                    lastDict.setdefault("end", True)
+                    newDict.setdefault("end", True)
                 lastDict = newDict
         print(letterDict)
 
         dp = [0]*(len(sentence)+1)
         for i in range(1, len(sentence)+1):
             dp[i] = dp[i-1] + 1
-            for idx in range(0, i):
-                if sentence[idx:i] in dictionary:
-                    dp[i] = min(dp[i], dp[idx])
+            # lastDict = letterDict
+            # 遍历 (i-1) - 0
+            #
+            # a = sententce[idx]
+            # 判断 a 是否在lastDict中，不在的话，直接结束判断
+            # 在的话，判断是否是end
+            #   是end的话，那么 进行判断
+            #   dp[i] = min(dp[i], dp[idx])
+            # 那么 lastDict = lastDict[a]
+            #
+            lastDict = letterDict
+            for idx in range(i-1, -1, -1):
+                letter = sentence[idx]
+                if letter in lastDict:
+                    lastDict = lastDict[letter]
+                    if lastDict.get("end", False):
+                        dp[i] = min(dp[i], dp[idx])
+                else:
+                    break
+        print(dp)
         return dp[len(sentence)]
 
 
 solution = Solution()
-dictionary = ["looked", "just", "like", "her", "brother"]
-sentence = "likejesslookedjustliketimherbrother"
+# dictionary = ["looked", "just", "like", "her", "brother"]
+# sentence = "likejesslookedjustliketimherbrother"
 # dictionary = ["frrrbbrrbfrfqqbbbrb","qr","b","rf","qqbbbbfrqbrrqrffbrqqqbqqfqfrr","r","ffqq","bffbqfqqbrrrf","fq","qfr","fr","rqrrbfbfq","r","f","qbqbrbrbqfqbbbfbbbfbq","bqqbbbqrbbrf","f"]
 # sentence = "bqqffbqbbfqrfrrrbbrrbfrfqqbbbrbfqfffffrfqfqfffffrrfqfrrqbqfrbfrqqrfrbrbbqbqbqqfqrfbfrfr"
+dictionary = ["wccm","wiw","uwwiwcmiiiwmmwicuwu","mw"]
+sentence = "iwiwwwmuiccwwwwwmumwwwmcciwwuiwcicwwuwicuiwciwmiwicwuwwmuimccwucuuim"
 solute = solution.respace(dictionary, sentence)
 print(solute)
