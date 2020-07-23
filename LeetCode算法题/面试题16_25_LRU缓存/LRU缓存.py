ef00f3c444bf84beff9f79575d6a8d287a2673d3
exueyuan <1079240024@qq.com>
2020-07-23 12:05:04 +0800

    新增LRU缓存的算法

---
+++ LeetCode算法题/面试题16_25_LRU缓存/LRU缓存.py
@@ -0,0 +1,50 @@
+class LRUCache(object):
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.capacity = capacity
+        self.capacyDictList = []
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        for position, capacyDict in enumerate(self.capacyDictList):
+            if key in capacyDict:
+                popDict = self.capacyDictList.pop(position)
+                self.capacyDictList.insert(0, popDict)
+                return popDict[key]
+
+        return -1
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        insertCapacyDict = {key: value}
+        for position, capacyDict in enumerate(self.capacyDictList):
+            if key in capacyDict:
+                self.capacyDictList.pop(position)
+                self.capacyDictList.insert(0, insertCapacyDict)
+                return
+
+        if len(self.capacyDictList) < self.capacity:
+            self.capacyDictList.insert(0, insertCapacyDict)
+        else:
+            self.capacyDictList.insert(0, insertCapacyDict)
+            self.capacyDictList.pop()
+
+lruCache = LRUCache(2)
+lruCache.put(1, 1)
+lruCache.put(2, 2)
+print(lruCache.get(1))
+lruCache.put(3, 3)
+print(lruCache.get(2))
+lruCache.put(4, 4)
+print(lruCache.get(1))
+print(lruCache.get(3))
+print(lruCache.get(4))
