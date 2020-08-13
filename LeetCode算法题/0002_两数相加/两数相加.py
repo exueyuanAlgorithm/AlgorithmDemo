ae127a2fe951a72f6a83d22d5d646dfba304d5a3
exueyuan <1079240024@qq.com>
2020-08-13 10:32:45 +0800

    新增部分代码

---
+++ LeetCode算法题/0002_两数相加/两数相加.py
@@ -0,0 +1,21 @@
+import ListNode
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        l3 = ListNode()
+        l3.val = (l1.val + l2.val) % 10
+        l3.next = 4
+        return l3
+
---
+++ LeetCode算法题/0207_课程表/课程表.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        prerequisiteDict = {
+
+        }
+        for prerequi in prerequisites:
+            if prerequi[0]
