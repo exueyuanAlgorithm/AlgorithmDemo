ffb0d804ef561c8c7f5bb10da3dff30eaa7d99c0
duyi <1079240024@qq.com>
2021-03-26 18:06:33 +0800

    132模式进行修改

--- LeetCode算法题/0456_132模式/132模式.py
+++ LeetCode算法题/0456_132模式/132模式.py
@@ -1,23 +1,26 @@
 class Solution(object):
 
-    # def juhe_qujian(self, ):
-
+    def juhe_qujian(self, lt_num_list, lt_num, left_equal):
+        if left_equal:
+            last_lt_num = lt_num_list.pop()
+            lt_num_list.append([last_lt_num[0], lt_num[1]])
+        else:
+            is_add = False
+            for lt_num_2 in lt_num_list:
+                if lt_num[0] <= lt_num_2[0] and lt_num[1] >= lt_num_2[1]:
+                    lt_num_2[0] = lt_num[0]
+                    lt_num_2[1] = lt_num[1]
+                    is_add = True
+                    break
+            if not is_add:
+                lt_num_list.append(lt_num)
     def find132pattern(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
         lt_num_list = []
         left_equal = False
-
         for i, num1 in enumerate(nums):
             for lt_num in lt_num_list:
-                if lt_num[2]:
-                    if lt_num[0] <= num1 < lt_num[1]:
-                        return True
-                else:
-                    if lt_num[0] < num1 < lt_num[1]:
-                        return True
+                if lt_num[0] < num1 < lt_num[1]:
+                    return True
 
             if i == len(nums) - 1:
                 return False
@@ -25,11 +28,11 @@ class Solution(object):
             # print(num1, num2)
             if num1 < num2:
                 # 存储num1，num2
-                lt_num_list.append([num1, num2, left_equal])
+                self.juhe_qujian(lt_num_list, [num1, num2], left_equal)
                 left_equal = True
                 # print(lt_num_list)
             elif num1 > num2:
                 left_equal = False
 
 solution = Solution()
-print(solution.find132pattern([-1,2, 2,0,3, 2]))
+print(solution.find132pattern([-1,2, 2,-4,-3, 2]))
